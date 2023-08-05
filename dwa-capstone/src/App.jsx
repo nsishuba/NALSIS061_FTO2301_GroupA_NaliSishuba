933ad010d7d6d1e0f0dc2bf156b81841c59106d9
nsishuba <nalisishuba@gmail.com>
2023-08-05 16:07:10 +0200

    Initial

---
+++ dwa-capstone/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState, useEffect } from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom" 
+import supabase from "./supabase" 
+import Login from "./pages/Login"
+import Home from "./pages/Home"
+import Favourite from './pages/Favourite'
+import './App.css'
+
+function App() {
+
+
+	return( 
+
+    
+  )
+}
+
+export default App
